2c859c330b18dcd5e8ed8493f8af8c7008901fa0
Mahmoud Agamy <agamyv@gmail.com>
2018-11-15 08:49:18 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,11 @@ def countdown(starting_num)
 end
 
 sleep(num_secs)
+
+def countdown(starting_num)
+  while starting_num > 0 
+    puts "#{starting_num} SECOND(S)!"
+    starting_num -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
